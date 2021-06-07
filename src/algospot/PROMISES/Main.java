857fac58039043fd663f7c0e3cd5f35913996e2d
kyjun719 <kyj1991719@naver.com>
2021-06-08 01:28:14 +0900

    알고스팟 문제 추가

---
+++ src/algospot/PROMISES/Main.java
@@ -0,0 +1,82 @@
+package algospot.PROMISES;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+	private static int INF = 987654321;
+	private static int[][] adj = new int[201][201];
+	private static int v;
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			int c = Integer.parseInt(br.readLine());
+			for(int tc = 0; tc < c; tc++) {
+				int[] tmp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+				v = tmp[0];
+				int m = tmp[1];
+				int n = tmp[2];
+
+				for(int i = 0; i < v; i++) {
+					Arrays.fill(adj[i], INF);
+				}
+				for(int i = 0; i < v; i++) {
+					adj[i][i]=0;
+				}
+
+				for(int i = 0; i < m; i++) {
+					int[] info = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+					int a = info[0];
+					int b = info[1];
+					int v = Math.min(info[2], adj[a][b]);
+
+					adj[a][b] = v;
+					adj[b][a] = v;
+				}
+
+				for(int k = 0; k < v; k++) {
+					for(int x = 0; x < v; x++) {
+						for(int y = 0; y < v; y++) {
+							adj[x][y] = Math.min(adj[x][y], adj[x][k]+adj[k][y]);
+						}
+					}
+				}
+
+				int ans = 0;
+				for(int i = 0; i < n; i++) {
+					int[] info = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+					if(!isUpdate(info[0], info[1], info[2])) {
+						ans++;
+					}
+				}
+
+				System.out.println(ans);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static boolean isUpdate(int a, int b, int c) {
+		//현재 계산된 간선보다 클경우 거짓 반환
+		if(adj[a][b] <= c) {
+			return false;
+		}
+
+		//x->y가 a,b를 이용하려면 x->a->b->y이거나 x->b->a->y이여야 함
+		for(int x = 0; x < v; x++) {
+			for(int y = 0; y < v; y++) {
+				adj[x][y] = Math.min(adj[x][y], Math.min(adj[x][a]+c+adj[b][y], adj[x][b]+c+adj[a][y]));
+			}
+		}
+
+		return true;
+	}
+}
