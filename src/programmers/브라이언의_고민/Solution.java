1b61fe3fbd4990e6c36b00d39e514f469a468241
kyjun719 <kyj1991719@naver.com>
2022-03-14 22:19:35 +0900

    프로그래머스 추가

--- src/programmers/브라이언의_고민/Solution.java
+++ src/programmers/브라이언의_고민/Solution.java
@@ -1,18 +1,18 @@
 package programmers.브라이언의_고민;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
-	static boolean[] used;
 	static int[][] idxArr;
+	static boolean[] used;
 	public String solution(String sentence) {
+		//소문자가 처음 나온 위치;
 		Queue<Character> charQueue = new LinkedList<>();
+		//소문자 등장갯수
 		int[] cnt = new int[26];
+		//규칙 사용여부
 		used = new boolean[26];
+		//소문자의 시작, 끝위치
 		idxArr = new int[26][2];
 		for(int i = 0; i < 26; i++) {
 			Arrays.fill(idxArr[i], -1);
@@ -27,24 +27,16 @@ class Solution {
 					charQueue.add(ch);
 					idxArr[ch-'a'][0] = i;
 				}
+				idxArr[ch-'a'][1] = i;
 				cnt[ch-'a']++;
 			} else if(ch == ' ') {
 				return "invalid";
 			}
 		}
 		
-		//소문자 끝위치 확인
-		for(int i = sentence.length()-1; i >= 0; i--) {
-			char ch = sentence.charAt(i);
-			if(ch >= 'a' && ch <= 'z') {
-				if(idxArr[ch-'a'][1] == -1) {
-					idxArr[ch-'a'][1] = i;
-				}
-			}
-		}
-		
 		String answer = "";
 		int lastIdx = -1;
+
 		while(!charQueue.isEmpty()) {
 			char ch = charQueue.poll();
 			if(used[ch-'a']) {
@@ -52,107 +44,137 @@ class Solution {
 			}
 			
 			String ret;
-			int e;
-			//2개 이상인 경우 규칙1, 2개인경우 규칙2
+			//2개가 아닌경우 규칙1, 2개인경우 규칙2
 			if(cnt[ch-'a'] != 2) {
 				//role 1
 				//규칙1인경우 시작-1~끝+1까지 확인
-				ret = parseString(sentence,ch,lastIdx,true);
-				e = idxArr[ch-'a'][1]+1;
+				ret = parseRule1String(sentence,ch,lastIdx);
+				lastIdx = idxArr[ch-'a'][1]+1;
 			} else {
 				//role 2
 				//규칙2인경우 시작+1~끝-1까지 확인
-				ret = parseString(sentence,ch,lastIdx,false);
-				e = idxArr[ch-'a'][1];
+				ret = parseRule2String(sentence,ch,lastIdx);
+				lastIdx = idxArr[ch-'a'][1];
 			}
+//			System.out.println(idxArr[ch-'a'][0]+","+lastIdx+">>"+ret);
 			
 			if(ret == null) {
 				return "invalid";
 			}
-			used[ch-'a'] = true;
 			answer += ret+" ";
-			lastIdx = e;
 		}
 		
-		System.out.println(lastIdx+">>"+sentence.length());
+//		System.out.println(lastIdx+">>"+sentence.length());
 		if(lastIdx != sentence.length()-1) {
 			String sub = sentence.substring(lastIdx+1);
 			for(char tmp : sub.toCharArray()) {
 				if(!isUpperCase(tmp)) {
 					return "invalid";
 				}
-				answer += tmp+" ";
+				answer += tmp;
 			}
 		}
+
 		if(answer.charAt(answer.length()-1) == ' ') {
 			answer = answer.substring(0, answer.length()-1);
 		}
 		return answer;
 	}
-	
-	
-	private String parseString(String sentence, char ch, int lastIdx, boolean isRule1) {
-		used[ch-'a'] = true;
-		int s,e;
-		if(isRule1) {
-			s = idxArr[ch-'a'][0]-1;
-			e = idxArr[ch-'a'][1]+1;
-		} else {
-			s = idxArr[ch-'a'][0]+1;
-			e = idxArr[ch-'a'][1]-1;
-		}
-		System.out.println(s+","+e+","+lastIdx+","+isRule1);
+
+	private String parseRule1String(String sentence, char ch, int beforeEndIdx) {
+		int s = idxArr[ch-'a'][0]-1;
+		int e = idxArr[ch-'a'][1]+1;
+
 		//위치를 넘어가는 경우 실패
-		if(s < 0 || s <= lastIdx || e > sentence.length() || !isUpperCase(sentence.charAt(s)) || !isUpperCase(sentence.charAt(e))) {
+		if(!canParse(sentence, s, e, beforeEndIdx)) {
 			return null;
 		}
-		
-		String answer = "";
-		if(lastIdx != -1 && lastIdx+(isRule1?1:2) < s) {
-			String sub = sentence.substring(lastIdx+1, s+(isRule1?1:-1));
-			for(char tmp : sub.toCharArray()) {
-				answer += tmp+" ";
+
+		StringBuilder ret = new StringBuilder(getBeforeRuleString(sentence, s, beforeEndIdx, true));
+
+//		System.out.println("parseRule1String sub:"+sentence.substring(s, e+1));
+
+		boolean isSmallChecked = true;
+		for(int i = s; i <= e; i++) {
+			char c = sentence.charAt(i);
+			if(isUpperCase(c)) {
+				if(!isSmallChecked) {
+					return null;
+				}
+				ret.append(c);
+				isSmallChecked = false;
+			} else {
+				if(c != ch) {
+					return null;
+				}
+				if(isSmallChecked) {
+					return null;
+				}
+				isSmallChecked = true;
 			}
 		}
-		
-		if(!isRule1) {
-			String ret = "";
-			boolean isRule1Checked = false;
-			for(int i = s; i <= e; i++) {
-				if(!isUpperCase(sentence.charAt(i))) {
-					if(isRule1Checked) {
-						return null;
-					}
-					
-					ret = parseString(sentence, sentence.charAt(i), lastIdx+1, true);
-					if(ret == null) {
-						return null;
-					}
-					i = idxArr[sentence.charAt(i)-'a'][1]+1;
-					isRule1Checked = true;
-				}
+
+		return ret.toString();
+	}
+
+	private String parseRule2String(String sentence, char ch, int beforeEndIdx) {
+		int s = idxArr[ch-'a'][0]+1;
+		int e = idxArr[ch-'a'][1]-1;
+
+		//위치를 넘어가는 경우 실패
+		if(!canParse(sentence, s, e, beforeEndIdx)) {
+			return null;
+		}
+
+		StringBuilder ret = new StringBuilder(getBeforeRuleString(sentence, s, beforeEndIdx, false));
+
+		//한단어에 규칙이 두개가 적용된경우 aXb...bXa형태
+		//규칙2안에 규칙1이 포함될 수 있음
+
+//		System.out.println("parseRule2String sub:"+sentence.substring(s, e+1));
+		if((s+1 <= e-1) && !isUpperCase(sentence.charAt(s+1)) && !isUpperCase(sentence.charAt(e-1))) {
+			String tmp = parseRule1String(sentence, sentence.charAt(s+1), s-1);
+
+			if(tmp == null) {
+				return null;
 			}
-			if(ret.isEmpty()) {
-				ret = sentence.substring(s,e+1);
-			}
-			answer += ret;
+
+			used[sentence.charAt(s+1)-'a'] = true;
+			ret.append(tmp);
 		} else {
 			for(int i = s; i <= e; i++) {
 				if(isUpperCase(sentence.charAt(i))) {
-					answer += sentence.charAt(i);
+					ret.append(sentence.charAt(i));
 				} else {
-					if(sentence.charAt(i) == ch) {
-						continue;
-					}
 					return null;
 				}
-				System.out.println(sentence.charAt(i)+">>"+answer);
 			}
 		}
-		
-		return answer;
+
+		return ret.toString();
 	}
 
+	private boolean canParse(String sentence, int s, int e, int beforeEndIdx) {
+		return s >= 0 && s > beforeEndIdx &&
+				e < sentence.length() &&
+				isUpperCase(sentence.charAt(s)) && isUpperCase(sentence.charAt(e));
+	}
+
+	private String getBeforeRuleString(String sentence, int s, int beforeEndIdx, boolean isRule1) {
+		int nc = isRule1 ? s-1 : s-2;
+
+		if(nc >= 0 && beforeEndIdx < nc) {
+//			System.out.println(s+"::"+beforeEndIdx+"~"+nc+">>"+sentence.substring(beforeEndIdx+1, nc+1));
+			StringBuilder sb = new StringBuilder();
+			for(int i = beforeEndIdx+1; i <= nc; i++) {
+				sb.append(sentence.charAt(i));
+			}
+			sb.append(' ');
+			return sb.toString();
+		}
+
+		return "";
+	}
 
 	private boolean isUpperCase(char ch) {
 		return ch >= 'A' && ch <= 'Z';
@@ -160,11 +182,38 @@ class Solution {
 
 
 	public static void main(String[] args) {
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println(new Solution().solution(br.readLine()));
-		} catch(Exception e) {
-			e.printStackTrace();
+		HashMap<String, List<String>> map = new HashMap<>();
+		map.put("HaEaLaLaObWORLDb", Arrays.asList("HELLO WORLD"));
+		map.put("SpIpGpOpNpGJqOqA", Arrays.asList("SIGONG JOA", "SIGONG J O A"));
+		map.put("AxAxAxAoBoBoB", Arrays.asList("invalid"));
+		map.put("aIaAM", Arrays.asList("I AM"));
+		map.put("AAAaBaAbBBBBbCcBdBdBdBcCeBfBeGgGGjGjGRvRvRvRvRvR", Arrays.asList("AAA B A BBBB C BBBB C BB GG G G G RRRRRR", "AA ABA BBBB C BBBB C BB GG GGG RRRRRR"));
+		map.put("aaA", Arrays.asList("invalid"));
+		map.put("Aaa", Arrays.asList("invalid"));
+		map.put("HaEaLaLaOWaOaRaLaD", Arrays.asList("invalid"));
+		map.put("aHELLOWORLDa", Arrays.asList("HELLOWORLD"));
+		map.put("HaEaLaLObWORLDb", Arrays.asList("HELL O WORLD"));
+		map.put("aHbEbLbLbOacWdOdRdLdDc", Arrays.asList("HELLO WORLD"));
+		map.put("abAba", Arrays.asList("invalid"));
+		map.put("HELLO WORLD", Arrays.asList("invalid"));
+		map.put("xAaAbAaAx", Arrays.asList("invalid"));
+		map.put("AbAaAbAaCa", Arrays.asList("invalid"));
+		map.put("AbAaAbAaC", Arrays.asList("invalid"));
+		map.put("a", Arrays.asList("invalid"));
+		map.put("aHbEbLbLbOacWdOdRdLdDcaHbEbLbLbOacWdOdRdLdDc", Arrays.asList("invalid"));
+		map.put("AaAaAcA", Arrays.asList("A A AA"));
+		map.put("abHELLObaWORLD", Arrays.asList("invalid"));
+		map.put("A B", Arrays.asList("invalid"));
+		map.put("AaAaAaAAAAAaAAAa", Arrays.asList("invalid"));
+		map.put("AaABbBCcC", Arrays.asList("AA BB CC"));
+		map.put("abAba", Arrays.asList("invalid"));
+		map.put("AbAaAbAaCa", Arrays.asList("invalid"));
+		map.put("AAAAbbbAAA", Arrays.asList("invalid"));
+
+		for(String key : map.keySet()) {
+			String ret = new Solution().solution(key);
+			System.out.println(key+","+ret+"="+(map.get(key).contains(ret)));
+			System.out.println();
 		}
 	}
 }
--- src/programmers/브라이언의_고민/intro.md
+++ src/programmers/브라이언의_고민/intro.md
@@ -2,30 +2,30 @@
 https://programmers.co.kr/learn/courses/30/lessons/1830
 
 input</br>
-HaEaLaLaObWORLDb
-SpIpGpOpNpGJqOqA
-AxAxAxAoBoBoB
-HELLOWORLD
-HaEaLaLaObWORLDb
-aHbEbLbLbOacWdOdRdLdDc
-aHELLOa bWORLDb
-HaEaLaLObWORLDb
-aHELLOWORLDa
-HaEaLaLaOWaOaRaLaD
-abHELLObaWORLD
+HaEaLaLaObWORLDb  
+SpIpGpOpNpGJqOqA  
+AxAxAxAoBoBoB  
+HELLOWORLD  
+HaEaLaLaObWORLDb  
+aHbEbLbLbOacWdOdRdLdDc  
+aHELLOa bWORLDb  
+HaEaLaLObWORLDb  
+aHELLOWORLDa  
+HaEaLaLaOWaOaRaLaD  
+abHELLObaWORLD  
 
 output</br>
-HELLO WORLD
-SIGONG JOA
-invalid
-HELLO WORLD
-HELLO WORLD
-HELLO WORLD
-invalid
-HELL O WORLD
-HELLOWORLD
-invalid
-invalid
+HELLO WORLD  
+SIGONG JOA  
+invalid  
+HELLO WORLD  
+HELLO WORLD  
+HELLO WORLD  
+invalid  
+HELL O WORLD  
+HELLOWORLD  
+invalid  
+invalid  
  
 #### 재정의와 추상화<br>
 -- 문제를 읽고 본인이 이해하기 쉬운 방식으로 해석<br>
@@ -34,10 +34,13 @@ invalid
 #### 계획 세우기<br>
 규칙1 또는 규칙2인지 확인하기 위해서 소문자의 갯수와 시작, 끝위치를 저장함.
 
-소문자 구간내 문자열이 규칙1 또는 규칙2에 맞으면 원문으로 변환을 하고, 아니면 종료함  
+소문자가 2개인경우 규칙2이고, 나머지는 규칙1이 맞는지 확인함.
+- 모든 규칙의 시작점은 이전 파싱완료 위치보다 커야하고, 시작점과 끝점은 대문자로 되어야함
+- 시작위치와 이전파싱종료 위치사이에 문자열이 있는 경우 정답에 추가하고 띄어쓰기를 더함
+  - 해당 문자열에 소문자가 있는 경우 다음 파싱때 실패가 나므로 전체적으로 실패로 반환됨
+- 규칙2 안에 규칙1이 될 수 있으므로 내부 문자열의 끝과 시작이 소문자인 경우 규칙1로 파싱함
+- 구간내 문자열이 규칙1 또는 규칙2에 맞으면 원문으로 변환을 하고, 아니면 종료함  
 
-남은 글자의 경우 공백을 두고 있던 문자열이므로 한글자당 공백을 하나씩 추가함
-
- 
+남은 글자의 경우 공백을 앞에 두고 모든 문자열을 추가함
 
 #### 계획 검증하기
